971028308087b3fe61edd5e1a65051a9b58dbe74
Jon Friese <jonfriese33@gmail.com>
2014-02-01 00:13:57 -0800

    added stackoverflow careers, did some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def manipulate_stackoverflow_xml(data)
+  	x = data["rss"]
+  	y = x["channel"]
+  	if y["totalResults"] == "0"
+  		return []
+  	else
+  		all_items = y["item"]
+  	end
+  end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,10 @@ respond_to :html, :json
   def index
     @keywords = params[:keywords].tr(" ", "+")
     @location = params[:location].tr(" ", "")
-    response = HTTParty.get("http://jobs.github.com/positions.json?description=#{@keywords}&location=#{@location}")
-    @jobs = JSON.parse(response.body)
+    github_response = HTTParty.get("http://jobs.github.com/positions.json?description=#{@keywords}&location=#{@location}")
+    stackoverflow_response = HTTParty.get("http://careers.stackoverflow.com/jobs/feed?searchTerm=#{@keywords}&location=#{@location}")
+    pre_stackoverflow = Hash.from_xml(stackoverflow_response)
+    @github_jobs = JSON.parse(github_response.body)
+    @stackoverflow_jobs = manipulate_stackoverflow_xml(pre_stackoverflow)
   end
 end
