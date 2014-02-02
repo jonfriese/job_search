c2de7f25704b5685bd83c9e8913c46fa0eff8c3e
Jon Friese <jonfriese33@gmail.com>
2014-02-02 00:47:00 -0800

    styling, UX

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,10 +6,14 @@ respond_to :html, :json
     @keywords = params[:keywords].tr(" ", "+")
     @location = params[:location].tr(" ", "")
     github_response = HTTParty.get("http://jobs.github.com/positions.json?description=#{@keywords}&location=#{@location}")
-    stackoverflow_response = HTTParty.get("http://careers.stackoverflow.com/jobs/feed?searchTerm=#{@keywords}&location=#{@location}")
-    ruby_now_response = HTTParty.get("http://feeds.feedburner.com/jobsrubynow?format=xml")
+    stackoverflow_response = HTTParty.get("http://careers.stackoverflow.com/jobs/feed?searchTerm=#{@keywords}&location=#{@location}&sort=p")
+    if %w(ruby rails).any? {|str| params[:keywords].downcase.include? str}
+      ruby_now_response = HTTParty.get("http://feeds.feedburner.com/jobsrubynow?format=xml")
+      @ruby_now_jobs = manipulate_xml(ruby_now_response)
+    else
+      @ruby_now_jobs = []
+    end
     pre_stackoverflow = Hash.from_xml(stackoverflow_response)
-    @ruby_now_jobs = manipulate_xml(ruby_now_response)
     @github_jobs = JSON.parse(github_response.body)
     @stackoverflow_jobs = manipulate_xml(pre_stackoverflow)
   end
