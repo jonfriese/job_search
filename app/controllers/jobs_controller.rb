87c5c8e0f1f645a8557e6ff05d259625b3a8f38a
Jon Friese <jonfriese33@gmail.com>
2014-02-02 22:44:01 -0800

    authentic jobs added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,8 @@ respond_to :html, :json
     @keywords = params[:keywords].tr(" ", "+")
     @location = params[:location].tr(" ", "")
     github_response = HTTParty.get("http://jobs.github.com/positions.json?description=#{@keywords}&location=#{@location}")
-    stackoverflow_response = HTTParty.get("http://careers.stackoverflow.com/jobs/feed?searchTerm=#{@keywords}&location=#{@location}&sort=p")
+    stackoverflow_response = HTTParty.get("http://careers.stackoverflow.com/jobs/feed?searchTerm=#{@keywords}&location=#{@location}")
+    authentic_response = HTTParty.get("http://www.authenticjobs.com/api/?api_key=#{ENV["AUTHENTIC_JOBS_API_KEY"]}&method=aj.jobs.search&keywords=#{@keywords}&location=#{@location}&perpage=20&begin_date=#{1.week.ago.to_i}&format=json")
     if %w(ruby rails).any? {|str| params[:keywords].downcase.include? str}
       ruby_now_response = HTTParty.get("http://feeds.feedburner.com/jobsrubynow?format=xml")
       if @location.empty?
@@ -24,7 +25,8 @@ respond_to :html, :json
       @ruby_now_jobs = []
     end
     pre_stackoverflow = Hash.from_xml(stackoverflow_response)
+    @authentic_jobs = JSON.parse(authentic_response.body)
     @github_jobs = JSON.parse(github_response.body)
-    @stackoverflow_jobs = manipulate_xml(pre_stackoverflow)
+    @stackoverflow_jobs = manipulate_xml(pre_stackoverflow).sort_by { |job| job["updated"] }.reverse
   end
 end
